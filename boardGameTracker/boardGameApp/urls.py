5aeefb7527f493be24ee3de8385f85530f1953e2
EmilK97 <eemil.kklonowski@gmail.com>
2025-04-26 11:39:31 +0200

    Fixed logout error again

--- boardGameTracker/boardGameApp/urls.py
+++ boardGameTracker/boardGameApp/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('library/', views.library_view, name='library'),
     path('add-game/', views.add_game_view, name='add_game'),
     path('game/<str:game_id>/', views.game_details_view, name='game_details'),
